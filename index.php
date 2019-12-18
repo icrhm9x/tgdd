e4b4cd9c452c3575e78359227c795c2307733d5e
icrhm9x <53967323+icrhm9x@users.noreply.github.com>
2019-12-18 23:30:06 +0700

    final carousel

--- index.php
+++ index.php
@@ -27,46 +27,40 @@ require_once DOC_ROOT . '/tgdd/common/php/common.php';
             <div class="js-carousel Carousel">
                 <div class="js-carouselCtx Carousel-content">
                     <div class="js-carouselList Carousel-content-list">
-                        <a class="Carousel-content-item" id="carousel1" href="#">
+                        <a class="Carousel-content-item" id="carousel1" data-index="1" href="#">
                             <img src="images/banner/carousel_01-800x300.png" alt="banner">
                         </a>
-                        <a class="Carousel-content-item" id="carousel2" href="#">
+                        <a class="Carousel-content-item" id="carousel2" data-index="2" href="#">
                             <img src="images/banner/carousel_02-800x300.png" alt="banner">
                         </a>
-                        <a class="Carousel-content-item" id="carousel3" href="#">
+                        <a class="Carousel-content-item" id="carousel3" data-index="3" href="#">
                             <img src="images/banner/carousel_03-800x300.png" alt="banner">
                         </a>
-                        <a class="Carousel-content-item" id="carousel4" href="#">
+                        <a class="Carousel-content-item" id="carousel4" data-index="4" href="#">
                             <img src="images/banner/carousel_04-800x300.png" alt="banner">
                         </a>
-                        <a class="Carousel-content-item" id="carousel5" href="#">
+                        <a class="Carousel-content-item" id="carousel5" data-index="5" href="#">
                             <img src="images/banner/carousel_05-800x300.png" alt="banner">
                         </a>
-                        <a class="Carousel-content-item" id="carousel6" href="#">
-                            <img src="images/banner/carousel_06-800x300.png" alt="banner">
-                        </a>
                     </div>
-                    <div class="Carousel-content__btn"></div>
-                    <div class="Carousel-content__btn Carousel-content__btn--right"></div>
+                    <div class="js-carouselBtnPrev Carousel-content__btn Carousel-content__btn--prev">‹</div>
+                    <div class="js-carouselBtnNext Carousel-content__btn Carousel-content__btn--next">›</div>
                 </div>
                 <div class="Carousel-nav">
-                    <div class="js-carouselNavItem Carousel-nav-item" data-index="1" >
-                        <p class="Carousel-nav-item__txt">1</p>
+                    <div class="js-carouselNavItem Carousel-nav-item Carousel-nav-item--active" data-index="1" >
+                        <p class="only-pc Carousel-nav-item__txt">Lên Đời Note 10<br>Trợ Giá Đến 3,5 Triệu</p>
                     </div>
                     <div class="js-carouselNavItem Carousel-nav-item" data-index="2">
-                        <p class="Carousel-nav-item__txt">2</p>
+                        <p class="only-pc Carousel-nav-item__txt">Sắm iPhone Online<br>Tặng Voucher Đến 3 Triệu</p>
                     </div>
                     <div class="js-carouselNavItem Carousel-nav-item" data-index="3">
-                        <p class="Carousel-nav-item__txt">3</p>
+                        <p class="only-pc Carousel-nav-item__txt">OPPO Reno2 F<br>Lì Xì Ngay 500 Ngàn</p>
                     </div>
                     <div class="js-carouselNavItem Carousel-nav-item" data-index="4">
-                        <p class="Carousel-nav-item__txt">4</p>
+                        <p class="only-pc Carousel-nav-item__txt">Đặt Trước Galaxy A51<br>Rước Bộ Quà 1,5 Triệu</p>
                     </div>
                     <div class="js-carouselNavItem Carousel-nav-item" data-index="5">
-                        <p class="Carousel-nav-item__txt">5</p>
-                    </div>
-                    <div class="js-carouselNavItem Carousel-nav-item" data-index="6">
-                        <p class="Carousel-nav-item__txt">6</p>
+                        <p class="only-pc Carousel-nav-item__txt">Vsmart Live<br>Trợ Giá Đến 3,5 Triệu</p>
                     </div>
                 </div>
             </div>
--- top/js/script.js
+++ top/js/script.js
@@ -1,5 +1,7 @@
 $(function() {
     // Handler for .ready() called.
+
+    // sub menu
     $('.js-navHamburger').click(function(){
         $('.js-nav').toggleClass('Nav--active');
         if($('.js-nav').hasClass('Nav--active')){
@@ -9,24 +11,138 @@ $(function() {
         }
     });
 
+    // ************************* Carousel *************************
+
     var carousel = $('.js-carousel');
     var carouselCtx = $('.js-carouselCtx');
+    var carouselList = $('.js-carouselList');
+    var itemLength = $('.js-carouselList .Carousel-content-item').length;
+    var currentItem = 1;
+    var currentX = 0;
+    var isScrolling = false;
+
+
+    // calculate height carousel
     $(window).on('load resize', function(){
         var mWidth = carousel.width();
         var mHeight = mWidth*0.375;
         carouselCtx.height(mHeight);
     });
 
-    var carouselList = $('.js-carouselList');
+    // default img
+    $('.js-carouselList img').on('mousedown', function(e){
+        e.preventDefault();
+    });
+
+    setUpCarousel();
+
+    function setUpCarousel(){
+        var realFirstItem = carouselList.find('.Carousel-content-item').first();
+        var firstItem = realFirstItem.clone();
+        var firstItemIndex = itemLength + 1;
+        firstItem.attr('id',"carousel"+firstItemIndex);
+        firstItem.addClass('Cloned');
+
+        var lastItem = carouselList.find('.Carousel-content-item').last().clone();
+        var lastItemIndex = 0;
+        lastItem.attr('id',"carousel"+lastItemIndex);
+        lastItem.addClass('Cloned');
+
+        carouselList.append(firstItem);
+        carouselList.prepend(lastItem);
+        carouselList.css('left', 0 - realFirstItem.position().left);
+    }
+
+    carouselList.on('mousedown', function(e){
+        currentX = e.pageX;
+        startX = currentX;
+        window.addEventListener('mousemove', carouselGrabbing);
+        window.addEventListener('mouseup', carouselStop);
+    });
+
+    function carouselGrabbing(e){
+        var distanceX = e.pageX - currentX;
+        currentX = e.pageX;
+        carouselList.css({
+            left : carouselList.position().left + distanceX
+        });
+        if(e.pageX - startX >= 50){
+            currentItem--;
+            scrollCarousel(currentItem);
+            window.removeEventListener('mousemove', carouselGrabbing);
+            window.removeEventListener('mouseup', carouselStop);
+        }else if(startX - e.pageX >= 50){
+            currentItem++;
+            scrollCarousel(currentItem);
+            window.removeEventListener('mousemove', carouselGrabbing);
+            window.removeEventListener('mouseup', carouselStop);
+        }
+    }
+    function carouselStop(e){
+        window.removeEventListener('mousemove', carouselGrabbing);
+        window.removeEventListener('mouseup', carouselStop);
+    }
+
+
     $('.js-carouselNavItem').click(function(){
-        var index = $(this).attr('data-index');
+        if($(this).hasClass('Carousel-nav-item--active')){
+            return;
+        }
+        currentItem = $(this).attr('data-index');
+        scrollCarousel(currentItem);
+    });
+
+    $('.js-carouselBtnPrev').click(function(){
+        if(isScrolling){
+            return;
+        }
+        currentItem--;
+        scrollCarousel(currentItem);
+    });
+    $('.js-carouselBtnNext').click(function(){
+        if(isScrolling){
+            return;
+        }
+        currentItem++;
+        scrollCarousel(currentItem);
+    });
+
+    function scrollCarousel(index){
+        if(isScrolling){
+            return;
+        }
+        isScrolling = true;
         var carouselItem = $('#carousel'+index);
+        if(carouselItem.length == 0){
+            isScrolling = false;
+            return;
+        }
+        if(carouselItem.hasClass('Cloned')){
+            index = carouselItem.attr('data-index');
+            var realItem = $('#carousel'+index);
+            currentItem = index;
+        }
+
+        $('.Carousel-nav-item--active').removeClass('Carousel-nav-item--active');
+        $('.js-carouselNavItem[data-index='+index+']').addClass('Carousel-nav-item--active');
         carouselList.animate({
             'left' : 0-carouselItem.position().left
-        }, 200);
-    });
+        }, 400, function(){
+            if(carouselItem.hasClass('Cloned')){
+                carouselList.css('left', 0 - realItem.position().left);
+            }
+            isScrolling = false;
+        });
+    }
+
+    // update left carousel when responsive
     $(window).on('resize', function(){
-        var left = 0-$('#carousel3').position().left;
+        var left = 0-$('#carousel'+currentItem).position().left;
         carouselList.css('left',left);
     });
+
+    // auto scroll
+    setInterval(function(){
+        scrollCarousel(currentItem++);
+    }, 5000);
   });
